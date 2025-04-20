420d32b34bf2cc9feeb2f9d11c81fd7ec3eb5723
Jose Mauricio Granados Muñoz <josegra@microsoft.com>
2025-04-20 10:06:19 -0600

    Addding more

---
+++ app/dashboard/counter/page.tsx
@@ -0,0 +1,4 @@
+const page = () => {
+  return <div> Counter Page</div>;
+};
+export default page;
---
+++ app/dashboard/main/page.tsx
@@ -0,0 +1,5 @@
+export const page = () => {
+  return <div>MAIN</div>;
+};
+
+export default page;
